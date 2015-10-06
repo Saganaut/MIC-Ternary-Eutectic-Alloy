e5eff87353dbed95a7f3cf3743c4a26292db0704
saganaut <robert.s.pienta@gmail.com>
2015-10-05 23:30:24 -0400

    newer pipeline works much better

--- smart_pipeline.py
+++ smart_pipeline.py
@@ -12,6 +12,7 @@ from pymks.stats import correlate
 from pymks.tools import draw_correlations
 from pymks.tools import draw_microstructures
 from pymks import MKSHomogenizationModel
+from pymks.tools import draw_component_variance
 
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LinearRegression
@@ -22,6 +23,7 @@ def load_data(filename):
   # The line below is because the matlab format has to be "unstructed"
   return shuffle_ndarray(data[data.keys()[0]])
 
+
 # The pymks takes a different format of data than our .mat data
 def shuffle_ndarray(data):
   n,m,k = data.shape
@@ -30,6 +32,7 @@ def shuffle_ndarray(data):
     new_data[i, :, :] = data[:, :, i]
   return new_data
 
+
 # Opens the metadata csv in data/metadata.csv, necessary for reading %Ag, %Cu, v etc..
 def load_metadata(filename):
   metadata = []
@@ -37,7 +40,6 @@ def load_metadata(filename):
     metareader = csv.reader(csvfile, delimiter='\t', quotechar='|')
     next(metareader, None)
     for row in metareader:
-      print row
       metadata.append({'ag':float(row[0]),
                        'cu':float(row[1]),
                        'sv':float(row[2]), 
@@ -46,14 +48,6 @@ def load_metadata(filename):
                        'filename':row[5]})
   return metadata
 
-# Gets the periodic DFT correlations for a single 2D face of the solid.
-def get_correlations_for_slice(al_data_slice):
-  # prim basis tellis it to use 0,1,2 as the 3 Stats
-  prim_basis = PrimitiveBasis(n_states=3, domain=[0,2])
-  disc_basis = prim_basis.discretize(al_data_slice)
-  # get the correlations
-  correlations = correlate(disc_basis, periodic_axes=(0, 1))
-  return correlations
 
 # Extracts the "best slice" of the block,
 # Currently takes the slice 10th from the end
@@ -66,38 +60,34 @@ def get_best_slice(al_data_slice):
 if __name__ == '__main__':
   # Load the metadata
   print "-->Loading Metadata"
-  metadata = load_metadata('data/metadata.tsv')
+  metadata = load_metadata('data/test.tsv')
   
   # Set up the inputs and output containers 
   samples = len(metadata)
-  x=np.ndarray(shape=(samples, 6*metadata[0]['x']**2))
+  x=np.ndarray(shape=(samples, metadata[0]['x'], metadata[0]['y']))
   y=np.ndarray(shape=(samples, 3)) 
 
   # For each sample sim in our dataset:
-  print "-->Running 2-pt Stats"
+  print "-->Constructing X"
+  i = 0
   for metadatum in metadata:
     # Load data frames
     print "--->Loading: " + metadatum['filename']
     metadatum['data'] = load_data('data/'+metadatum['filename'])
     # Get a representative slice from the block (or ave or whatever we decide on)
     best_slice = get_best_slice(metadatum['data'])
-    # Get 2-pt Stats for the best slice
-    print "--->Getting 2pt stats"
-    metadatum['stats'] = get_correlations_for_slice(best_slice)
-  
-  print metadata[0]['stats'].shape
-  # Construct X and Y for PCA and linkage
-  print "-->Creating X and Y"
-  i = 0
-  for metadatum in metadata:
-    x[i,0:6*metadatum['x']**2] = metadatum['stats'].flatten()
- 
-  
+    x[i] = best_slice 
+    y[i,0] = metadatum['ag']
+    y[i,1] = metadatum['cu']
+    y[i,2] = metadatum['sv']
+    i += 1
+    
+
   prim_basis = PrimitiveBasis(n_states=3, domain=[0,2])
-  x_ = prim_basis.discretize(metadata[0]['data'])
-  x_corr = correlate(x_)
-  draw_correlations(x_corr.real)
-  quit()
+  x_ = prim_basis.discretize(x)
+  x_corr = correlate(x_, periodic_axes=[0, 1])
+  #uncomment to view one containers
+  #draw_correlations(x_corr[0].real)
 
   # Reduce all 2-pt Stats via PCA
   # Try linear reg on inputs and outputs
@@ -107,5 +97,6 @@ if __name__ == '__main__':
                                  property_linker=linker,
                                  compute_correlations=False)
   model.n_components = 40
-  model.fit(metadatum['stats'], y, periodic_axes=[0, 1]) 
+  model.fit(x_corr, y, periodic_axes=[0, 1]) 
   print model.reduced_fit_data
+  draw_component_variance(model.dimension_reducer.explained_variance_ratio_)
