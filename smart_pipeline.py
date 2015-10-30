2347875877fda8f7341db9829f5a1d5b91c51127
Saganaut <robert.s.pienta@gmail.com>
2015-10-30 02:21:48 -0400

    significant improvements

--- smart_pipeline.py
+++ smart_pipeline.py
@@ -19,17 +19,16 @@ from pymks.tools import draw_component_variance
 from pymks.tools import draw_components
 from pymks.tools import draw_gridscores_matrix
 
-from sklearn.cross_validation import train_test_split, cross_val_score
+from sklearn import linear_model, svm, tree
+from sklearn.cross_validation import train_test_split, cross_val_score, LeaveOneOut
 from sklearn.decomposition import PCA
-from sklearn import linear_model
-from sklearn.cross_validation import LeaveOneOut
 from sklearn.grid_search import GridSearchCV
-from sklearn import svm, tree
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.pipeline import Pipeline
 
 
 # Just loads the data from the .mat file in dropbox
-def load_data(filename):    
+def load_data(filename):
   data = sio.loadmat(filename)
   # The line below is because the matlab format has to be "unstructed"
   #print data['phase_field_solid']
@@ -55,9 +54,9 @@ def load_metadata(filename):
       if float(row[2]) > .2: continue
       metadata.append({'ag':float(row[0]),
                        'cu':float(row[1]),
-                       'sv':float(row[2]), 
-                       'x':int(row[3]), 
-                       'y':int(row[4]), 
+                       'sv':float(row[2]),
+                       'x':int(row[3]),
+                       'y':int(row[4]),
                        'filename':row[5]+'.mat'})
   return metadata
 
@@ -73,16 +72,16 @@ def plot_component_variance(x, y):
   prim_basis = PrimitiveBasis(n_states=3, domain=[0, 2])
   model = MKSHomogenizationModel(basis=prim_basis)
   model.n_components = 20
-  model.fit(x, y, periodic_axes=[0, 1]) 
+  model.fit(x, y, periodic_axes=[0, 1])
   # Draw the plot containing the PCA variance accumulation
   draw_component_variance(model.dimension_reducer.explained_variance_ratio_)
 
-def run_conventional_linkage(x, y, n_comps, linker_model, verbose=2):
-  loo_cv = LeaveOneOut(x.shape[0])
-  print "--->Cross validating"
-  cvs = cross_val_score(linker_model, x, y, cv=5, scoring='r2', verbose=verbose)  
-  mse = cross_val_score(linker_model, x, y, cv=5, scoring='mean_squared_error', verbose=verbose)  
-  print np.mean(cvs), np.mean(mse)
+def run_conventional_linkage(x, y, n_comps, linker_model, verbose=0, k_folds=5):
+  print "---->Cross validating"
+  cvs = cross_val_score(linker_model, x, y, cv=k_folds, scoring='r2', verbose=verbose)
+  mse = cross_val_score(linker_model, x, y, cv=k_folds, scoring='mean_squared_error', verbose=verbose)
+  print '---->R2: ', np.mean(cvs)
+  print '---->MSE: ', np.mean(mse)
   return np.mean(cvs), np.std(cvs), np.mean(mse), np.std(mse)
 
 def plot_components(x, y, n_comps, linker_model, verbose=2):
@@ -93,43 +92,31 @@ def plot_components(x, y, n_comps, linker_model, verbose=2):
   model.fit(x,y,periodic_axes=[0,1])
 
   print model.property_linker.coef_
-  draw_components([model.reduced_fit_data[0:3, :2], 
+  draw_components([model.reduced_fit_data[0:3, :2],
                    model.reduced_fit_data[3:6, :2],
                    model.reduced_fit_data[6:9, :2],
-                   model.reduced_fit_data[9:11, :2], 
+                   model.reduced_fit_data[9:11, :2],
                    model.reduced_fit_data[11:14, :2],
                    model.reduced_fit_data[14:16, :2],
                    model.reduced_fit_data[16:17, :2],
                    model.reduced_fit_data[17:18, :2]],
-                   ['Ag:0.237	Cu:0.141	v:0.0525', 
-                    'Ag:0.237	Cu:0.141	v:0.0593', 
+                   ['Ag:0.237	Cu:0.141	v:0.0525',
+                    'Ag:0.237	Cu:0.141	v:0.0593',
                     'Ag:0.237	Cu:0.141	v:0.0773',
-                    'Ag:0.237	Cu:0.141	v:0.0844', 
-                    'Ag:0.239	Cu:0.138	v:0.0791', 
-                    'Ag:0.239	Cu:0.138	v:0.0525', 
-                    'Ag:0.237	Cu:0.141	v:0.0914', 
-                    'Ag:0.237	Cu:0.141	v:0.0512']) 
+                    'Ag:0.237	Cu:0.141	v:0.0844',
+                    'Ag:0.239	Cu:0.138	v:0.0791',
+                    'Ag:0.239	Cu:0.138	v:0.0525',
+                    'Ag:0.237	Cu:0.141	v:0.0914',
+                    'Ag:0.237	Cu:0.141	v:0.0512'])
 
 
-def run_gridcv_linkage(x, y):
-
-
-  X_train, X_test, y_train, y_test = train_test_split(x.reshape(flat_shape), y,
-                                                    test_size=0.1, random_state=0)
-  print 'Training set size: ', (X_train.shape)
-  print 'Testing set size: ', (X_test.shape)
-
- #params_to_tune = {'degree': np.arange(1, 4), 'n_components': np.arange(1, 8)}
-  params_to_tune = {'degree': np.arange(1, 2), 'n_components': np.arange(3, 5)}
-  fit_params = {'size': x[0].shape, 'periodic_axes': [0, 1]}
-  loo_cv = LeaveOneOut(X_train.shape[0])
-  gs = GridSearchCV(model, params_to_tune, cv=loo_cv, n_jobs=-1, fit_params=fit_params, scoring='mean_squared_error').fit(X_train, y_train)
-  print('Order of Polynomial'), (gs.best_estimator_.degree)
-  print('Number of Components'), (gs.best_estimator_.n_components)
-  print('R-squared Value'), (gs.score(X_test, y_test))
-  
-  #draw_gridscores_matrix(gs, ['n_components', 'degree'], score_label='R-Squared',
-  #                     param_labels=['Number of Components', 'Order of Polynomial'])
+def run_gridcv_linkage(x, y, model, params_to_tune, k_folds=5):
+  # fit_params = {'periodic_axes':[0, 1]}
+  gs = GridSearchCV(model, params_to_tune, cv=k_folds, n_jobs=2, scoring='mean_squared_error').fit(x, y)
+  return gs
+  # print('Order of Polynomial'), (gs.best_estimator_.degree)
+  # print('Number of Components'), (gs.best_estimator_.n_components)
+  # print('R-squared Value'), (gs.score(X_test, y_test))
 
 def compute_correlations(x):
   print "-->Constructing Correlations"
@@ -143,6 +130,14 @@ def compute_correlations(x):
     row_ctr += 1
   return x_corr, x_corr_flat
 
+def test_polynomial_fits(x, y, n_comps, model, k_folds=5):
+  for i in range(1,6):
+    poly = PolynomialFeatures(degree=i)
+    poly_x = poly.fit_transform(x)
+    r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(poly_x, y, n_comps, model)
+    print r2_mean, r2_std, mse_mean, mse_std
+    print
+
 def compute_pca_scores(x_flat):
   pca = PCA(n_components=5)
   x_pca = pca.fit(x_flat).transform(x_flat)
@@ -151,7 +146,7 @@ def compute_pca_scores(x_flat):
 
   #~~~~~~~MAIN~~~~~~~
 if __name__ == '__main__':
-  
+
   if os.path.isfile('cache/x_y_data.pgz'):
     print "-->Pickle found, loading x and y directly"
     with gzip.GzipFile('cache/x_y_data.pgz', 'r') as f:
@@ -161,12 +156,12 @@ if __name__ == '__main__':
     # Load the metadata
     print "-->Loading Metadata"
     metadata = load_metadata('data/metadata_all.tsv')
-    
-    # Set up the inputs and output containers 
+
+    # Set up the inputs and output containers
     samples = len(metadata)
     x=np.ndarray(shape=(samples, metadata[0]['x'], metadata[0]['y']))
-    y=np.ndarray(shape=(samples, 3)) 
-    solid_vel=np.ndarray(shape=(samples, 3)) 
+    y=np.ndarray(shape=(samples, 3))
+    solid_vel=np.ndarray(shape=(samples, 3))
 
     # For each sample sim in our dataset:
     print "-->Constructing X"
@@ -177,7 +172,7 @@ if __name__ == '__main__':
       al_chunk = load_data('data/test/'+metadatum['filename'])
       # Get a representative slice from the block (or ave or whatever we decide on)
       best_slice = get_best_slice(al_chunk)
-      x[i] = best_slice 
+      x[i] = best_slice
       y[i,0] = metadatum['ag']
       y[i,1] = metadatum['cu']
       y[i,2] = metadatum['sv']
@@ -189,46 +184,49 @@ if __name__ == '__main__':
 
   # PCA component variance plot
   #plot_component_variance(x, y[:,2])
-  
+
   # Plot components in pca space
 #  plot_components(x,y, 5, linear_model.LinearRegression())
 
-  if os.path.isfile('cache/x_corr.pgz'):
-    with gzip.GzipFile('cache/x_corr.pgz', 'r') as f:
-      print '-->Correlations found unpickling'
-      x_corr, x_corr_flat = pickle.load(f)
-  else:
-    # Get correlations
-    x_corr, x_corr_flat = compute_correlations(x)
-    with gzip.GzipFile('cache/x_corr.pgz', 'w') as f:
-      pickle.dump((x_corr, x_corr_flat), f)
-  
+  x_corr, x_corr_flat = compute_correlations(x)
   # Use PCA on flattened correlations
   x_pca = compute_pca_scores(x_corr_flat)
-# LINEAR 
-  print "-->Homogenizing"
+# LINEAR
+  print "-->Optimizing Linkers"
   with open('data/linker_results_r2_data.csv', 'w') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter=',')
 
     with open('data/linker_results_mse_data.csv', 'w') as csv_file_mse:
       csv_writer_mse = csv.writer(csv_file_mse, delimiter=',')
       print "--->LinearRegression"
-      linker = linear_model.LinearRegression() 
-      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,linker)
+      linker = linear_model.LinearRegression()
+      params = {'fit_intercept':(True, False)}
+      opt_model = run_gridcv_linkage(y,x_pca,linker,params)
+      print('---->Use Intercept: '), (opt_model.best_estimator_.fit_intercept)
+      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,opt_model)
       csv_writer.writerow(['LinearRegression', r2_mean, r2_std])
       csv_writer_mse.writerow(['LinearRegression', mse_mean, mse_std])
-     
+
+      test_polynomial_fits(y,x_pca,5,opt_model)
+      print
       print "--->Lasso"
-      linker = linear_model.Lasso() 
-      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,linker)
+      linker = linear_model.Lasso()
+      params = {'alpha':(.0001,.001,.01,.1,1,10,100)}
+      opt_model = run_gridcv_linkage(y,x_pca,linker,params)
+      print('---->Best Alpha: '), (opt_model.best_estimator_.alpha)
+      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,opt_model)
       csv_writer.writerow(['Lasso', r2_mean, r2_std])
       csv_writer_mse.writerow(['Lasso', mse_mean, mse_std])
-      
+      print
       print "--->Ridge"
-      linker = linear_model.Ridge() 
-      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,linker)
+      linker = linear_model.Ridge()
+      params = {'alpha':(.0001,.001,.01,.1,1,10,100)}
+      opt_model = run_gridcv_linkage(y,x_pca,linker,params)
+      print('---->Best Alpha: '), (opt_model.best_estimator_.alpha)
+      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,opt_model)
       csv_writer.writerow(['Ridge', r2_mean, r2_std])
       csv_writer_mse.writerow(['Ridge', mse_mean, mse_std])
+      print
 
       # NON linear
 #      print "--->LinearSVR"
@@ -236,24 +234,21 @@ if __name__ == '__main__':
 #      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,linker)
 #      csv_writer.writerow(['LinearSVR', r2_mean, r2_std])
 #      csv_writer_mse.writerow(['LinearSVR', mse_mean, mse_std])
-      
+
 #      print "--->nuSVR"
-#      linker = svm.NuSVR() 
+#      linker = svm.NuSVR()
 #      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,linker)
 #      csv_writer.writerow(['nuSVR', r2_mean, r2_std])
 #      csv_writer_mse.writerow(['nuSVR', mse_mean, mse_std])
-      
+
       print "--->TreeRegressor"
-      linker = tree.DecisionTreeRegressor() 
-      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,linker)
+      linker = tree.DecisionTreeRegressor()
+      params = {'max_depth':np.arange(2,50), 'splitter':('best', 'random')}
+      opt_model = run_gridcv_linkage(y,x_pca,linker,params)
+      print('---->Best max_depth,splitter:'), (opt_model.best_estimator_.max_depth, opt_model.best_estimator_.splitter)
+      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,opt_model)
       csv_writer.writerow(['TreeRegressor', r2_mean, r2_std])
       csv_writer_mse.writerow(['TreeRegressor', mse_mean, mse_std])
-      
-      print "--->RandomTreeRegressor"
-      linker = tree.ExtraTreeRegressor() 
-      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,linker)
-      csv_writer.writerow(['RandomForest', r2_mean, r2_std])
-      csv_writer_mse.writerow(['RandomForest', mse_mean, mse_std])
 
   quit()
 
@@ -266,51 +261,51 @@ if __name__ == '__main__':
   x_corr_flat = np.ndarray(shape=(samples,  x_corr.shape[1]*x_corr.shape[2]*x_corr.shape[3]))
   row_ctr = 0
   for row in x_corr:
-    x_corr_flat[row_ctr] = row.flatten() 
- 
+    x_corr_flat[row_ctr] = row.flatten()
+
   print x.shape
   flat_len = (x.shape[0],) + (np.prod(x.shape[1:]),)
   X_train, X_test, y_train, y_test = train_test_split(x.reshape(flat_len), y,
                                                     test_size=0.2, random_state=3)
   print(x_corr.shape)
-  print(X_test.shape) 
+  print(X_test.shape)
   # uncomment to view one containers
   #draw_correlations(x_corr[0].real)
-  
+
   # Reduce all 2-pt Stats via PCA
   # Try linear reg on inputs and outputs
   reducer = PCA(n_components=3)
-  linker = LinearRegression() 
+  linker = LinearRegression()
   model = MKSHomogenizationModel(basis=prim_basis,
                                  compute_correlations=False)
-  
-  #model.fit(x_corr, y, periodic_axes=[0, 1]) 
-  # set up parameters to optimize 
+
+  #model.fit(x_corr, y, periodic_axes=[0, 1])
+  # set up parameters to optimize
   params_to_tune = {'degree': np.arange(1, 4), 'n_components': np.arange(1, 8)}
   fit_params = {'size':x_corr_flat.shape, 'periodic_axes': [0, 1]}
   loo_cv = LeaveOneOut(samples)
   gs = GridSearchCV(model, params_to_tune, cv=loo_cv, n_jobs=6, fit_params=fit_params).fit(x_corr_flat, y)
 
   # Manual fit
-  #model.fit(x_corr, y, periodic_axes=[0, 1]) 
+  #model.fit(x_corr, y, periodic_axes=[0, 1])
   #print model.reduced_fit_data
 
   # Draw the plot containing the PCA variance accumulation
   #draw_component_variance(model.dimension_reducer.explained_variance_ratio_)
 
-  draw_components([model.reduced_fit_data[0:3, :2], 
+  draw_components([model.reduced_fit_data[0:3, :2],
                    model.reduced_fit_data[3:6, :2],
                    model.reduced_fit_data[6:9, :2],
-                   model.reduced_fit_data[9:11, :2]], ['0.0525', '0.0593', '0.0773','0.0844']) 
+                   model.reduced_fit_data[9:11, :2]], ['0.0525', '0.0593', '0.0773','0.0844'])
   print('Order of Polynomial'), (gs.best_estimator_.degree)
   print('Number of Components'), (gs.best_estimator_.n_components)
-  print('R-squared Value'), (gs.score(X_test, y_test)) 
- 
-  #draw_components([model.reduced_fit_data[0:3, :2], 
+  print('R-squared Value'), (gs.score(X_test, y_test))
+
+  #draw_components([model.reduced_fit_data[0:3, :2],
   #                 model.reduced_fit_data[3:6, :2],
   #                 model.reduced_fit_data[6:9, :2],
   #                 model.reduced_fit_data[9:11, :2],
-  #                 model.reduced_fit_data[11:, :2]], ['0.0525', '0.0593', '0.0773','0.0844', '>0.6']) 
-  
+  #                 model.reduced_fit_data[11:, :2]], ['0.0525', '0.0593', '0.0773','0.0844', '>0.6'])
+
 
 
