3ef2646166e9125be706b84b9b176075bd5a63a5
saganaut <robert.s.pienta@gmail.com>
2015-11-17 17:06:29 -0500

    sexy plot

--- smart_pipeline.py
+++ smart_pipeline.py
@@ -9,6 +9,10 @@ import h5py
 import cPickle as pickle
 import gzip
 
+from itertools import combinations
+
+import matplotlib.pyplot as plt
+
 # Import the methods for 2-pt stats
 from pymks import PrimitiveBasis
 from pymks.stats import correlate
@@ -25,7 +29,7 @@ from sklearn.decomposition import PCA
 from sklearn.grid_search import GridSearchCV
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.pipeline import Pipeline
-
+from sklearn.ensemble import RandomForestClassifier
 
 # Just loads the data from the .mat file in dropbox
 def load_data(filename):
@@ -38,7 +42,7 @@ def load_data(filename):
 # The pymks takes a different format of data than our .mat data
 def shuffle_ndarray(data):
   n,m,k = data.shape
-  new_data = np.zeros((k,n,m))
+  new_data = np.zeros((k,n,m), dtype=np.uint8)
   for i in xrange(k):
     new_data[i, :, :] = data[:, :, i]
   return new_data
@@ -84,6 +88,56 @@ def run_conventional_linkage(x, y, n_comps, linker_model, verbose=0, k_folds=5):
   print '---->MSE: ', np.mean(mse)
   return np.mean(cvs), np.std(cvs), np.mean(mse), np.std(mse)
 
+def plot_sample_time_variation(block, n_comps=3):
+  import matplotlib as mpl
+  import matplotlib.cm as cm
+  print '-->Plotting Time Variation'
+  if os.path.isfile('cache/corr_example.pgz'):
+    print "-->Pickle found, loading corr directly"
+    with gzip.GzipFile('cache/corr_example.pgz', 'r') as f:
+      x_pca = pickle.load(f)
+  else:
+    n = block.shape[0]
+    sample_ind = range(0,301,5)
+    corrs, corrs_flat = compute_correlations(block[sample_ind,:,:])
+    pca = PCA(n_components=n_comps)
+    x_pca = pca.fit(corrs_flat).transform(corrs_flat)
+    with gzip.GzipFile('cache/corr_example.pgz', 'w') as f:
+      pickle.dump(x_pca, f)
+  t = range(0,301,5)
+  x = [xi[0] for xi in x_pca]
+  y = [xi[1] for xi in x_pca]
+  norm = mpl.colors.Normalize(vmin=min(t), vmax=max(t))
+  mapper = cm.ScalarMappable(norm=norm, cmap=cm.cool)
+  colors = [mapper.to_rgba(ti) for ti in t]
+
+  f, (ax1, ax2, ax3) = plt.subplots(3, 1)
+  for ax in [ax1, ax2, ax3]:
+    for target in ['x', 'y']:
+      ax.tick_params(
+        axis=target,       # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        left='off',
+        right='off',
+        labelbottom='on')  # labels along the bottom edge are off 
+  ax1.scatter(x, y, color=colors,alpha=0.5, edgecolors='black')
+  ax1.set_xlabel('PCA Score 1')
+  ax1.set_ylabel('PCA Score 2')
+  ax2.plot(t, x)
+  ax2.set_xlabel('Time')
+  ax2.set_ylabel('PC1')
+  ax3.plot(t, y)
+  ax3.set_xlabel('Time')
+  ax3.set_ylabel('PC2')
+  
+  plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+  plt.savefig('PCA_over_block.png')
+  quit()
+  
+
+
 def plot_components(x, y, n_comps, linker_model, verbose=2):
   prim_basis = PrimitiveBasis(n_states=3, domain=[0, 2])
   model = MKSHomogenizationModel(basis=prim_basis,
@@ -109,6 +163,13 @@ def plot_components(x, y, n_comps, linker_model, verbose=2):
                     'Ag:0.237	Cu:0.141	v:0.0914',
                     'Ag:0.237	Cu:0.141	v:0.0512'])
 
+def plot_a_tree(clf):
+  from sklearn.externals.six import StringIO  
+  import pydot 
+  dot_data = StringIO() 
+  tree.export_graphviz(clf, out_file=dot_data)
+  graph = pydot.graph_from_dot_data(dot_data.getvalue()) 
+  graph.write_png("tree_example.png")
 
 def run_gridcv_linkage(x, y, model, params_to_tune, k_folds=5):
   # fit_params = {'periodic_axes':[0, 1]}
@@ -143,20 +204,55 @@ def compute_pca_scores(x_flat):
   x_pca = pca.fit(x_flat).transform(x_flat)
   return x_pca
 
+def test_correlation_combos(x,y):
+  print '-->Testing Correlations'
+  x_corr, x_corr_flat = compute_correlations(x)
+  subsets = [lam_x for lam_x in combinations(range(x_corr.shape[3]), 2)]
+  temp_x_corr_flat = np.ndarray(shape=(x.shape[0],  x_corr.shape[1]*x_corr.shape[2]*2))
+  results_mat = np.ndarray(shape=(x_corr.shape[3], x_corr.shape[3]))
+  n = x.shape[0]
+  for subset in subsets:
+    print '--->Testing correlations: '+str(subset)
+    for i in xrange(n):
+      # set the temporary flattened matrix to use only a pair of correlations
+      temp_x_corr_flat[i] = x_corr[i, :, :, subset].flatten()
+    x_pca = compute_pca_scores(temp_x_corr_flat)
+    # Train the model
+    linker = linear_model.LinearRegression()
+    params = {'fit_intercept':(True, False)}
+    opt_model = run_gridcv_linkage(y,x_pca,linker,params)
+    r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,opt_model)
+    results_mat[subset[0], subset[1]] = r2_mean
+    results_mat[subset[1], subset[0]] = r2_mean
+
+  # Plot the results
+  fig, ax = plt.subplots()
+  heatmap = ax.pcolor(results_mat, cmap=plt.cm.Blues)
+  fig.colorbar(heatmap)
+  ax.set_yticks(np.arange(results_mat.shape[0]) + 0.5, minor=False)
+  ax.set_xticks(np.arange(results_mat.shape[1]) + 0.5, minor=False)
+  labels = ['AlAl', 'AgAg', 'CuCu', 'AlAg', 'AlCu', 'AgCu']
+  ax.set_xticklabels(labels, minor=False)
+  ax.set_yticklabels(labels, minor=False)
+  ax.set_title('R^2 for pairs of correlations')
+  plt.show()
+
+
+
 
   #~~~~~~~MAIN~~~~~~~
 if __name__ == '__main__':
+  # Load the metadata
+  print "-->Loading Metadata"
+  metadata = load_metadata('data/metadata_all.tsv')
 
   if os.path.isfile('cache/x_y_data.pgz'):
     print "-->Pickle found, loading x and y directly"
     with gzip.GzipFile('cache/x_y_data.pgz', 'r') as f:
       x, y = pickle.load(f)
   else:
-    os.mkdir('cache')
-    # Load the metadata
-    print "-->Loading Metadata"
-    metadata = load_metadata('data/metadata_all.tsv')
-
+    if not os.path.isdir('cache'):
+      os.mkdir('cache') 
     # Set up the inputs and output containers
     samples = len(metadata)
     x=np.ndarray(shape=(samples, metadata[0]['x'], metadata[0]['y']))
@@ -174,13 +270,19 @@ if __name__ == '__main__':
       best_slice = get_best_slice(al_chunk)
       x[i] = best_slice
       y[i,0] = metadatum['ag']
-      y[i,1] = metadatum['cu']
-      y[i,2] = metadatum['sv']
+      #y[i,1] = metadatum['cu']
+      y[i,1] = metadatum['sv']
       solid_vel[i] = metadatum['sv']
       i += 1
     print "-->Pickling x and y"
     with gzip.GzipFile('cache/x_y_data.pgz', 'w') as f:
       pickle.dump((x,y), f)
+
+  # Test correlation params
+  #test_correlation_combos(x,y)
+
+  # Plot blocks time varying behavior in PCA space. 
+  plot_sample_time_variation(load_data('data/test/'+metadata[0]['filename']))
 
   # PCA component variance plot
   #plot_component_variance(x, y[:,2])
@@ -246,9 +348,19 @@ if __name__ == '__main__':
       params = {'max_depth':np.arange(2,50), 'splitter':('best', 'random')}
       opt_model = run_gridcv_linkage(y,x_pca,linker,params)
       print('---->Best max_depth,splitter:'), (opt_model.best_estimator_.max_depth, opt_model.best_estimator_.splitter)
+      plot_a_tree(opt_model.best_estimator_)
       r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,opt_model)
       csv_writer.writerow(['TreeRegressor', r2_mean, r2_std])
       csv_writer_mse.writerow(['TreeRegressor', mse_mean, mse_std])
+
+      print "--->RandomForest"
+      linker = RandomForestClassifier()
+      params = {'n_estimators':range(1,100,10)}
+      opt_model = run_gridcv_linkage(y,x_pca,linker,params)
+      print('---->n_est:'), (opt_model.best_estimator_.n_estimators)
+      r2_mean, r2_std, mse_mean, mse_std = run_conventional_linkage(y,x_pca,5,opt_model)
+
+ 
 
   quit()
 
